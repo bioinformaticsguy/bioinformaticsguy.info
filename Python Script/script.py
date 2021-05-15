7cac02b15c70f2603ff86a81e2a75bb6afb7d2c8
bioinformaticsguy <bioinforamticsguy@gmail.com>
2021-05-15 19:57:37 +0500

    Added RAW text to Python for Bioinforamtics Series

--- Python Script/script.py
+++ Python Script/script.py
@@ -2,8 +2,13 @@ string_without_line_breaks = ""
 
 with open('foo.txt', "r+") as file:
     for line in file:
-        stripped_line = line.rstrip()
+        stripped_line = line.rstrip() + " "
         string_without_line_breaks += stripped_line
+    
+    
+with open ("oneLine.md", "w") as file:
+    file.write(string_without_line_breaks)
+    
 
 
-print(string_without_line_breaks)
+# print(string_without_line_breaks)
